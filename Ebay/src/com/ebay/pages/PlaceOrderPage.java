5838a8ccf8c4d0632ee587cece70502e1248ca97
Sandeep Shetty <sandeep@Sandeeps-MacBook.local>
2019-02-12 20:46:35 +0530

    Üpdated as per feedback

--- Ebay/src/com/ebay/pages/PlaceOrderPage.java
+++ Ebay/src/com/ebay/pages/PlaceOrderPage.java
@@ -9,8 +9,6 @@ package com.ebay.pages;
 
 import org.openqa.selenium.By;
 
-import com.ebay.helpers.Utility;
-
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
@@ -19,13 +17,23 @@ public class PlaceOrderPage extends BasePage {
 	AndroidDriver<MobileElement> androidDriver;
 
 	public By menuButton = By.id("com.ebay.mobile:id/home");
+	public By homeButtonInMenu = By.id("com.ebay.mobile:id/menuitem_home");
+	public By notificationsButtonInMenu = By.id("com.ebay.mobile:id/menuitem_notifications");
+	public By messagesButtonInMenu = By.id("com.ebay.mobile:id/menuitem_messages");
 	public By settingsButton = By.id("com.ebay.mobile:id/menuitem_settings");
+	public By settingsPageHeader = By.xpath("//android.widget.TextView[@text='Settings']");
+	public By notificationsHeader = By.xpath("//android.widget.TextView[@content-desc='Notifications button']");
 	public By watchingButton = By.id("com.ebay.mobile:id/menuitem_watching");
 	public By categoriesButton = By.id("com.ebay.mobile:id/menuitem_categories");
 	public By toggleButton = By.id("android:id/switch_widget");
 	public By regionButton = By.xpath("//android.widget.TextView[@content-desc='Country/region button']");
+	public By autoDetectLabel = By.xpath("//android.widget.TextView[@text='Auto-Detect']");
+	public By chooseCountryDescription = By.xpath(
+			"//android.widget.TextView[contains(@text,'Choose my country/region based on my primary shipping address')]");
+	public By regionSelectorePageHeader = By.xpath("//android.widget.TextView[@text='Country/region']");
 	public By regionDropDown = By
 			.xpath("//android.widget.TextView[@text='Country/region' and @resource-id='android:id/title']");
+	public By selectedCountry = By.xpath("//android.widget.TextView[@text='Australia']");
 	public By searchFieldInSettings = By.id("com.ebay.mobile:id/filter");
 	public By countryName = By.id("com.ebay.mobile:id/check_country");
 	public By backButton = By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']");
@@ -54,25 +62,6 @@ public class PlaceOrderPage extends BasePage {
 	public PlaceOrderPage(AndroidDriver<MobileElement> androidDriver) {
 		super(androidDriver);
 		this.androidDriver = androidDriver;
-	}
-
-	/**
-	 * changeLocation() method enables us to change location in Settings. This
-	 * method has been added as the same is mentioned in the requirement email
-	 * 
-	 */
-
-	public void changeLocation() {
-		findAndroidElement(menuButton).click();
-		Utility.scroll(androidDriver, categoriesButton, watchingButton);
-		findAndroidElement(settingsButton).click();
-		findAndroidElement(regionButton).click();
-		findAndroidElement(toggleButton).click();
-		findAndroidElement(regionDropDown).click();
-		findAndroidElement(searchFieldInSettings).sendKeys("Australia");
-		findAndroidElement(countryName).click();
-		findAndroidElement(backButton).click();
-		findAndroidElement(backButton).click();
 	}
 
 }
--- Ebay/src/com/ebay/tests/PlaceOrderTest.java
+++ Ebay/src/com/ebay/tests/PlaceOrderTest.java
@@ -39,16 +39,120 @@ public class PlaceOrderTest extends BaseTest {
 		prop = Utility.getProperties();
 	}
 
-	@Test(priority = 1, description = "Change location to Australia and search for a product")
+	@Test(priority = 1, description = "Tap on Menu button")
+	public void clickMenuButton() {
+		landingPage.findAndroidElement(landingPage.menuButton).click();
+		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.homeButtonInMenu));
+	}
+
+	@DataProvider(name = "menuElements")
+	public Object[][] menuElements() {
+		Object[][] input = { { landingPage.homeButtonInMenu, "Home button not found" },
+				{ landingPage.notificationsButtonInMenu, "Notifications button not found" },
+				{ landingPage.messagesButtonInMenu, "Messages button not found" },
+				{ landingPage.watchingButton, "Watching button not found" },
+				{ landingPage.categoriesButton, "Categories button not found" } };
+		return input;
+	}
+
+	@Test(priority = 2, dataProvider = "menuElements", description = "Check elements in menu")
+	public void checkElementsInMenu(By element, String error) {
+		assertTrue(landingPage.isAndroidElementDisplayed(element), error);
+	}
+
+	@Test(priority = 3, description = "Change location to Australia and search for a product")
+	public void scrollDownAndClickSettingsButtonInMenu() {
+		Utility.scroll(androidDriver, landingPage.categoriesButton, landingPage.watchingButton);
+		landingPage.findAndroidElement(landingPage.settingsButton).click();
+		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.settingsPageHeader));
+	}
+
+	@DataProvider(name = "settingsPageElements")
+	public Object[][] settingsPageElements() {
+		Object[][] input = { { landingPage.settingsPageHeader, "Settings page header not found" },
+				{ landingPage.notificationsHeader, "Notifications header not found" },
+				{ landingPage.regionButton, "Region button not found" } };
+		return input;
+	}
+
+	@Test(priority = 4, dataProvider = "settingsPageElements", description = "Check elements in Settings page")
+	public void checkElementsInSettingsPage(By element, String error) {
+		assertTrue(landingPage.isAndroidElementDisplayed(element), error);
+	}
+
+	@Test(priority = 5, description = "Click region button")
+	public void clickRegionButton() {
+		landingPage.findAndroidElement(landingPage.regionButton).click();
+		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.toggleButton));
+	}
+
+	@Test(priority = 6, description = "Click the toggle button")
+	public void clickAutoDetectToggleButton() {
+		landingPage.findAndroidElement(landingPage.toggleButton).click();
+		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.regionDropDown));
+	}
+
+	@DataProvider(name = "regionSelectorPageElements")
+	public Object[][] regionSelectorPageElements() {
+		Object[][] input = { { landingPage.autoDetectLabel, "Auto detect label not found" },
+				{ landingPage.chooseCountryDescription, "Choose country description not found" },
+				{ landingPage.regionDropDown, "Region drop down not found" } };
+		return input;
+	}
+
+	@Test(priority = 7, dataProvider = "regionSelectorPageElements", description = "Check elements in region selector page")
+	public void checkElementsInrRegionSelectorPage(By element, String error) {
+		assertTrue(landingPage.isAndroidElementDisplayed(element), error);
+	}
+
+	@Test(priority = 8, description = "click region drop down in Settings")
+	public void clickRegionDropDown() {
+		landingPage.findAndroidElement(landingPage.regionDropDown).click();
+		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.searchFieldInSettings));
+	}
+
+	@DataProvider(name = "searchLocationPageElements")
+	public Object[][] searchLocationPageElements() {
+		Object[][] input = { { landingPage.regionSelectorePageHeader, "Page header not found" },
+				{ landingPage.searchFieldInSettings, "Search field not found" },
+				{ landingPage.countryName, "Country name not found" } };
+		return input;
+	}
+
+	@Test(priority = 9, dataProvider = "searchLocationPageElements", description = "Check elements in search location page")
+	public void checkElementsInrLocationSearchPage(By element, String error) {
+		assertTrue(landingPage.isAndroidElementDisplayed(element), error);
+	}
+
+	@Test(priority = 10, description = "Enter a search keyword")
+	public void searchForRegion() {
+		landingPage.findAndroidElement(landingPage.searchFieldInSettings).sendKeys("Australia");
+		assertEquals(landingPage.findAndroidElement(landingPage.countryName).getText(), "Australia");
+	}
+
+	@Test(priority = 11, description = "Select a region in search results")
+	public void selectRegion() {
+		landingPage.findAndroidElement(landingPage.countryName).click();
+		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.selectedCountry));
+	}
+
+	@Test(priority = 12, description = "Navigate back to home page")
+	public void navigateBackToHomePage() {
+		landingPage.findAndroidElement(landingPage.backButton).click();
+		landingPage.findAndroidElement(landingPage.backButton).click();
+		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.searchField));
+	}
+
+	@Test(priority = 13, description = "Change location to Australia and search for a product")
 	public void searchProduct() {
-		landingPage.changeLocation();
+//		landingPage.changeLocation();
 		landingPage.findAndroidElement(landingPage.searchField).click();
 		landingPage.findAndroidElement(landingPage.searchFieldInDetail).sendKeys(prop.getProperty("searchparam"));
 		Utility.androidElements(landingPage.searchSuggestion, androidDriver).get(0).click();
 		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.searchResult));
 	}
 
-	@Test(priority = 2, description = "Tap on a search result")
+	@Test(priority = 14, description = "Tap on a search result")
 	public void clickSearchResult() {
 		Utility.androidElements(landingPage.searchResult, androidDriver).get(0).click();
 		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.itemName));
@@ -62,12 +166,12 @@ public class PlaceOrderTest extends BaseTest {
 
 	}
 
-	@Test(priority = 3, dataProvider = "productDetailElements", description = "Check elements in product detail page")
+	@Test(priority = 15, dataProvider = "productDetailElements", description = "Check elements in product detail page")
 	public void checkElementsInProductDetail(By element, String error) {
-		assertTrue(landingPage.isAndroidElementDisplayed(element));
+		assertTrue(landingPage.isAndroidElementDisplayed(element), error);
 	}
 
-	@Test(priority = 4, description = "Save item name and price and tap on Buy Now button in product detail")
+	@Test(priority = 16, description = "Save item name and price and tap on Buy Now button in product detail")
 	public void tapBuyNowButton() {
 		map = new LinkedHashMap<String, String>();
 		map.put("itemName", landingPage.findAndroidElement(landingPage.itemName).getText());
@@ -76,7 +180,7 @@ public class PlaceOrderTest extends BaseTest {
 		assertTrue(landingPage.isAndroidElementDisplayed(landingPage.userName));
 	}
 
-	@Test(priority = 5, description = "Login and verify the price and item name in cart")
+	@Test(priority = 17, description = "Login and verify the price and item name in cart")
 	public void loginToEbay() {
 		landingPage.findAndroidElement(landingPage.userName).sendKeys(prop.getProperty("username"));
 		landingPage.findAndroidElement(landingPage.password).sendKeys(prop.getProperty("password"));
@@ -86,7 +190,7 @@ public class PlaceOrderTest extends BaseTest {
 		assertTrue(landingPage.findAndroidElement(landingPage.itemPrice).getText().contains(map.get("itemPrice")));
 	}
 
-	@Test(priority = 6, description = "Review the order in checkout page")
+	@Test(priority = 18, description = "Review the order in checkout page")
 	public void reviewOrder() {
 		map.put("shippingCharge", landingPage.findAndroidElement(landingPage.shippingPriceInCart).getText());
 		landingPage.findAndroidElement(landingPage.reviewButton).click();
@@ -95,7 +199,7 @@ public class PlaceOrderTest extends BaseTest {
 				landingPage.findAndroidElement(landingPage.itemPriceInCart).getText().contains(map.get("itemPrice")));
 	}
 
-	@Test(priority = 7, description = "Check pricing details in checkout page")
+	@Test(priority = 19, description = "Check pricing details in checkout page")
 	public void scrollDownToCheckFinalPrice() {
 		Utility.scroll(androidDriver, landingPage.payWithLabel, landingPage.messageToSellerLabel);
 		assertTrue(Utility.androidElements(landingPage.subTotal, androidDriver).get(0).getText()
